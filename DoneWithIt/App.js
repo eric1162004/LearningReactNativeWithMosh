904e8a004007b698b2a0af43702468b20d3c4b15
Eric Cheung <eric1162004@hotmail.com>
2021-04-24 02:39:31 -0700

    first commit'

---
+++ DoneWithIt/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+import MessagesScreen from './app/screens/MessagesScreen';
+
+export default function App() {
+  return (
+<MessagesScreen/> 
+  );
+}
